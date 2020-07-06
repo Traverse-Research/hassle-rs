4b1706a5439a8f66829590678653043526b846c2
Marijn Suijten <marijn@traverseresearch.nl>
2020-07-06 11:57:22 +0200

    os: Add BSTR type on non-Windows

--- src/os.rs
+++ src/os.rs
@@ -10,10 +10,13 @@ mod os_defs {
 mod os_defs {
     pub type CHAR = i8;
     pub type WCHAR = u32;
+    pub type OLECHAR = WCHAR;
     pub type LPSTR = *mut CHAR;
     pub type LPWSTR = *mut WCHAR;
     pub type LPCSTR = *const CHAR;
     pub type LPCWSTR = *const WCHAR;
+    pub type BSTR = *mut OLECHAR;
+    pub type LPBSTR = *mut BSTR;
     pub type HRESULT = LONG;
     pub type LONG = i64;
 }
