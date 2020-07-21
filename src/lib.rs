0272110c6b23b962e59ec4f0eee1a6a06f99d57e
Jasper Bekkers <bekkers@gmail.com>
2020-07-21 11:36:33 +0100

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
 #![allow(non_upper_case_globals)]
-
 #![allow(
     clippy::transmute_ptr_to_ptr, // Introduced by com-rs
     clippy::too_many_arguments, // We're wrapping and API outside of our control
