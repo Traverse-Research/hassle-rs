Traverse-Research/hassle-rs

commit 94fbad7330cfe39a92168f8ff40867189b6266db

Support libdxcompiler.so on Linux (#5)
* Type include_handler as *const IDxcIncludeHandler instead of c_void

* os: Add BSTR type on non-Windows

* lib: Fix bitflags macro include

extern crate requires deprecated `#[macro_use]`, which should be
replaced with `use` instead.

* Make intellisense compile on Linux

* [LINUX ONLY] intellisense: Insert the usual destructor dummies

* [WINDOWS] Make OS wrapper types compatible with Windows again

HRESULT is 32-bit: https://en.wikipedia.org/wiki/HRESULT

* Delete dummy functions (virtual dtors) again

This assumes a DXC compiled without virtual destructor! Which - as it
stands - is invalid with undefined behaviour, but "works". This needs
followup in DXC itself.

This includes a revert of commit 308dd8625c8eb05d95cf0800918bb418ce837ca5.

* [WIN/LINUX] Implement vtable dummies (virtual dtors) through extra trait

TODO: Might as well keep the IUnknown name and "hide" the COM one (less
changes, as long as the right one is use'd.

* [LINUX] wrapper: Library is in PWD

* Add error type

* Fix return type of IDxcType::get_kind

* ffi: Replace c_void with the respective interface type

get_buffer_pointer returns a mutable (non-const) void ptr,
create_include_handler returns an IDxcIncludeHandler, and annotate some
c_void types. All the remaining c_void uses are that way in the official
API.

* Cargo.toml: Only include winapi dependency on cfg(windows)

Using winapi on not(windows) is a no-op already, but saves pulling in
this extra dependency.

* [LINUX] Disable DXIL validation at compile-time

* README: Mention libdxcompiler.so requirement on Linux

* Improve error handling

* Add more context about the .dll / .so we're trying to load

* Expose helper on Linux as well even though it always returns an error, makes the API slightly easier to use

Co-authored-by: Tomasz Stachowiak <h3@h3.gd>
Co-authored-by: Jasper Bekkers <bekkers@gmail.com>

examples/intellisense-tu.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let args = vec![];
 
-    let dxc = Dxc::new();
+    let dxc = Dxc::new().unwrap();
 
     let intellisense = dxc.create_intellisense().unwrap();
 
@@ -18,7 +18,7 @@ fn main() {
     let unsaved_file = intellisense.create_unsaved_file(name, source).unwrap();
 
     let translation_unit = index
-        .parse_translation_unit(name, &args, &vec![&unsaved_file], local_options)
+        .parse_translation_unit(name, &args, &[&unsaved_file], local_options)
         .unwrap();
 
     let cursor = translation_unit.get_cursor().unwrap();

src/utils.rs
@@ -1,39 +1,39 @@
+use crate::os::{BSTR, HRESULT, LPSTR, LPWSTR, WCHAR};
 use crate::wrapper::*;
-use std::rc::Rc;
-use winapi::shared::ntdef::LPSTR;
-use winapi::shared::ntdef::LPWSTR;
-use winapi::shared::wtypes::BSTR;
+use thiserror::Error;
+
+#[cfg(windows)]
 use winapi::um::oleauto::{SysFreeString, SysStringLen};
 
-pub(crate) fn to_wide(msg: &str) -> Vec<u16> {
-    use std::ffi::OsStr;
-    use std::iter::once;
-    use std::os::windows::ffi::OsStrExt;
-
-    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
-    wide
+pub(crate) fn to_wide(msg: &str) -> Vec<WCHAR> {
+    widestring::WideCString::from_str(msg).unwrap().into_vec()
 }
 
 pub(crate) fn from_wide(wide: LPWSTR) -> String {
-    use std::ffi::OsString;
-    use std::os::windows::ffi::OsStringExt;
-
-    let len = unsafe { (0..).take_while(|&i| *wide.offset(i) != 0).count() };
-
-    OsString::from_wide(unsafe { std::slice::from_raw_parts(wide, len) })
-        .into_string()
-        .unwrap()
+    unsafe {
+        widestring::WideCStr::from_ptr_str(wide)
+            .to_string()
+            .expect("utf16 decode failed")
+    }
 }
 
+#[cfg(windows)]
 pub(crate) fn from_bstr(string: BSTR) -> String {
     unsafe {
         let len = SysStringLen(string);
-        let slice: &[u16] = ::std::slice::from_raw_parts(string, len as usize);
+        let slice: &[WCHAR] = ::std::slice::from_raw_parts(string, len as usize);
         let result = String::from_utf16(slice).unwrap();
         SysFreeString(string);
-
-        return result;
+        result
     }
+}
+
+#[cfg(not(windows))]
+pub(crate) fn from_bstr(string: BSTR) -> String {
+    // TODO (Marijn): This does NOT cover embedded NULLs:
+    // https://docs.microsoft.com/en-us/windows/win32/api/oleauto/nf-oleauto-sysstringlen#remarks
+    // Fortunately BSTRs are only used in names currently, which _likely_ don't include NULL characters (like binary data)
+    from_lpstr(string as LPSTR)
 }
 
 pub(crate) fn from_lpstr(string: LPSTR) -> String {
@@ -61,6 +61,25 @@ impl DxcIncludeHandler for DefaultIncludeHandler {
     }
 }
 
+#[derive(Error, Debug)]
+pub enum HassleError {
+    #[error("Win32 error: {0}")]
+    Win32Error(HRESULT),
+    #[error("Compile error: {0}")]
+    CompileError(String),
+    #[error("Validation error: {0}")]
+    ValidationError(String),
+    #[error("Error loading library {filename:?} / {inner:?}")]
+    LoadLibraryError {
+        filename: String,
+        inner: libloading::Error,
+    },
+    #[error("LibLoading error: {0:?}")]
+    LibLoadingError(#[from] libloading::Error),
+    #[error("Windows only")]
+    WindowsOnly(String),
+}
+
 /// Helper function to directly compile a HLSL shader to an intermediate language,
 /// this function expects `dxcompiler.dll` to be available in the current
 /// executable environment.
@@ -73,17 +92,15 @@ pub fn compile_hlsl(
     target_profile: &str,
     args: &[&str],
     defines: &[(&str, Option<&str>)],
-) -> Result<Vec<u8>, String> {
-    let dxc = Dxc::new();
+) -> Result<Vec<u8>, HassleError> {
+    let dxc = Dxc::new()?;
 
-    let compiler = dxc.create_compiler().unwrap();
-    let library = dxc.create_library().unwrap();
-
-    let source = Rc::new(String::from(shader_text));
+    let compiler = dxc.create_compiler()?;
+    let library = dxc.create_library()?;
 
     let blob = library
-        .create_blob_with_encoding_from_str(Rc::clone(&source))
-        .unwrap();
+        .create_blob_with_encoding_from_str(shader_text)
+        .map_err(HassleError::Win32Error)?;
 
     let result = compiler.compile(
         &blob,
@@ -97,11 +114,16 @@ pub fn compile_hlsl(
 
     match result {
         Err(result) => {
-            let error_blob = result.0.get_error_buffer().unwrap();
-            Err(library.get_blob_as_string(&error_blob))
+            let error_blob = result
+                .0
+                .get_error_buffer()
+                .map_err(HassleError::Win32Error)?;
+            Err(HassleError::CompileError(
+                library.get_blob_as_string(&error_blob),
+            ))
         }
         Ok(result) => {
-            let result_blob = result.get_result().unwrap();
+            let result_blob = result.get_result().map_err(HassleError::Win32Error)?;
 
             Ok(result_blob.to_vec())
         }
@@ -109,22 +131,30 @@ pub fn compile_hlsl(
 }
 
 /// Helper function to validate a DXIL binary independant from the compilation process,
-/// this function expected `dxcompiler.dll` and `dxil.dll` to be available in the current
+/// this function expects `dxcompiler.dll` and `dxil.dll` to be available in the current
 /// execution environment.
-pub fn validate_dxil(data: &[u8]) -> Result<Vec<u8>, String> {
-    let dxc = Dxc::new();
-    let dxil = Dxil::new();
+/// `dxil.dll` is currently not available on Linux.
+pub fn validate_dxil(data: &[u8]) -> Result<Vec<u8>, HassleError> {
+    let dxc = Dxc::new()?;
+    let dxil = Dxil::new()?;
 
-    let validator = dxil.create_validator().unwrap();
-    let library = dxc.create_library().unwrap();
+    let validator = dxil.create_validator()?;
+    let library = dxc.create_library()?;
 
-    let blob_encoding = library.create_blob_with_encoding(&data).unwrap();
+    let blob_encoding = library
+        .create_blob_with_encoding(&data)
+        .map_err(HassleError::Win32Error)?;
 
     match validator.validate(blob_encoding.into()) {
         Ok(blob) => Ok(blob.to_vec()),
         Err(result) => {
-            let error_blob = result.0.get_error_buffer().unwrap();
-            Err(library.get_blob_as_string(&error_blob))
+            let error_blob = result
+                .0
+                .get_error_buffer()
+                .map_err(HassleError::Win32Error)?;
+            Err(HassleError::ValidationError(
+                library.get_blob_as_string(&error_blob),
+            ))
         }
     }
 }

Cargo.toml
@@ -24,13 +24,14 @@ maintenance = { status = "actively-developed" }
 default-target = "x86_64-pc-windows-msvc"
 
 [dependencies]
-libloading = "0.5.2"
-com-rs = "0.2.0"
-winapi = "0.3.7"
-bitflags = "1.1.0"
+libloading = "0.6.2"
+com-rs = "0.2.1"
+bitflags = "1.2.1"
+widestring = "0.4.2"
+thiserror = "1.0"
+
+[target.'cfg(windows)'.dependencies]
+winapi = { version = "0.3.9", features = ["wtypes", "oleauto"] }
 
 [dev-dependencies]
 rspirv = "0.6.0"
-
-[features]
-default = ["winapi/wtypes", "winapi/oleauto"]

examples/file-ast.rs
@@ -77,7 +77,7 @@ fn main() {
 
     let args = vec![];
 
-    let dxc = Dxc::new();
+    let dxc = Dxc::new().unwrap();
 
     let intellisense = dxc.create_intellisense().unwrap();
 
@@ -88,7 +88,7 @@ fn main() {
     let unsaved_file = intellisense.create_unsaved_file(name, source).unwrap();
 
     let translation_unit = index
-        .parse_translation_unit(name, &args, &vec![&unsaved_file], local_options)
+        .parse_translation_unit(name, &args, &[&unsaved_file], local_options)
         .unwrap();
 
     let cursor = translation_unit.get_cursor().unwrap();

examples/validate.rs
@@ -1,6 +1,7 @@
 use hassle_rs::*;
 
 #[repr(C)]
+#[repr(packed)]
 pub struct MinimalHeader {
     four_cc: u32,
     hash_digest: [u32; 4],
@@ -8,7 +9,7 @@ pub struct MinimalHeader {
 
 // zero_digest & get_digest from https://github.com/gwihlidal/dxil-signing/blob/master/rust/src/main.rs
 
-fn zero_digest(buffer: &mut [u8]) -> () {
+fn zero_digest(buffer: &mut [u8]) {
     let buffer_ptr: *mut u8 = buffer.as_mut_ptr();
     let header_ptr: *mut MinimalHeader = buffer_ptr as *mut _;
     let header_mut: &mut MinimalHeader = unsafe { &mut *header_ptr };
@@ -39,11 +40,11 @@ fn main() {
     zero_digest(&mut dxil);
 
     let without_digest = get_digest(&dxil);
+    println!("Before validation: {:?}", without_digest);
 
     let validated_dxil = validate_dxil(&dxil).unwrap();
 
     let with_digest = get_digest(&validated_dxil);
 
-    println!("Before validation: {:?}", without_digest);
     println!("After validation: {:?}", with_digest);
 }

src/ffi.rs
@@ -1,16 +1,16 @@
 #![allow(clippy::transmute_ptr_to_ptr)]
 #![allow(clippy::too_many_arguments)]
 
+use crate::os::{HRESULT, LPCWSTR, LPWSTR};
+pub(crate) use crate::unknown::IDxcUnknownShim;
 use com_rs::{com_interface, iid, IUnknown, IID};
 use std::ffi::c_void;
-use winapi::shared::ntdef::{LPCWSTR, LPWSTR};
-use winapi::shared::winerror::HRESULT;
 
 pub type DxcCreateInstanceProc =
     extern "system" fn(rclsid: &IID, riid: &IID, ppv: *mut *mut c_void) -> HRESULT;
 
 pub type DxcCreateInstanceProc2 = extern "system" fn(
-    malloc: *const c_void,
+    malloc: /* IMalloc */ *const c_void,
     rclsid: &IID,
     riid: &IID,
     ppv: *mut *mut c_void,
@@ -18,17 +18,17 @@ pub type DxcCreateInstanceProc2 = extern "system" fn(
 
 iid!(pub IID_IDxcBlob = 0x8BA5_FB08, 0x5195, 0x40e2, 0xAC, 0x58, 0x0D, 0x98, 0x9C, 0x3A, 0x01, 0x02);
 com_interface! {
-    interface IDxcBlob: IUnknown{
+    interface IDxcBlob: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcBlob,
         vtable: IDxcBlobVtbl,
-        fn get_buffer_pointer() -> *const c_void;
+        fn get_buffer_pointer() -> *mut c_void;
         fn get_buffer_size() -> usize;
     }
 }
 
 iid!(pub IID_IDxcBlobEncoding = 0x7241_d424, 0x2646, 0x4191, 0x97, 0xc0, 0x98, 0xe9, 0x6e, 0x42, 0xfc, 0x68);
 com_interface! {
-    interface IDxcBlobEncoding: IDxcBlob, IUnknown{
+    interface IDxcBlobEncoding: IDxcBlob, IDxcUnknownShim, IUnknown {
         iid: IID_IDxcBlobEncoding,
         vtable: IDxcBlobEncodingVtbl,
         fn get_encoding(known: *mut u32, code_page: *mut u32) -> HRESULT;
@@ -37,7 +37,7 @@ com_interface! {
 
 iid!(pub IID_IDxcLibrary = 0xe520_4dc7, 0xd18c, 0x4c3c, 0xbd, 0xfb, 0x85, 0x16, 0x73, 0x98, 0x0f, 0xe7);
 com_interface! {
-    interface IDxcLibrary: IUnknown{
+    interface IDxcLibrary: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcLibrary,
         vtable: IDxcLibraryVtbl,
         fn set_malloc(malloc: *const c_void) -> HRESULT;
@@ -45,9 +45,9 @@ com_interface! {
         fn create_blob_from_file(filename: LPCWSTR, code_page: *const u32, blob_encoding: *mut *mut IDxcBlobEncoding) -> HRESULT;
         fn create_blob_with_encoding_from_pinned(text: *const c_void, size: u32, code_page: u32, blob_encoding: *mut *mut IDxcBlobEncoding) -> HRESULT;
         fn create_blob_with_encoding_on_heap_copy(text: *const c_void, size: u32, code_page: u32, blob_encoding: *mut *mut IDxcBlobEncoding) -> HRESULT;
-        fn create_blob_with_encoding_on_malloc(text: *const c_void, malloc: *const c_void, size: u32, code_page: u32, blob_encoding: *mut *mut IDxcBlobEncoding) -> HRESULT;
-        fn create_include_handler(include_handler: *mut *mut c_void) -> HRESULT;
-        fn create_stream_from_blob_read_only(blob: *const IDxcBlob, stream: *mut *mut c_void) -> HRESULT;
+        fn create_blob_with_encoding_on_malloc(text: *const c_void, malloc: /* IMalloc */ *const c_void, size: u32, code_page: u32, blob_encoding: *mut *mut IDxcBlobEncoding) -> HRESULT;
+        fn create_include_handler(include_handler: *mut *mut IDxcIncludeHandler) -> HRESULT;
+        fn create_stream_from_blob_read_only(blob: *const IDxcBlob, stream: /* IStream */ *mut *mut c_void) -> HRESULT;
         fn get_blob_as_utf8(blob: *const IDxcBlob, blob_encoding: *mut *mut IDxcBlobEncoding) -> HRESULT;
         fn get_blob_as_utf16(blob: *const IDxcBlob, blob_encoding: *mut *mut IDxcBlobEncoding) -> HRESULT;
     }
@@ -55,7 +55,7 @@ com_interface! {
 
 iid!(pub IID_IDxcOperationResult = 0xCEDB_484A, 0xD4E9, 0x445A, 0xB9, 0x91, 0xCA, 0x21, 0xCA, 0x15, 0x7D, 0xC2);
 com_interface! {
-    interface IDxcOperationResult: IUnknown{
+    interface IDxcOperationResult: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcOperationResult,
         vtable: IDxcOperationResultVtbl,
         fn get_status(status: *mut u32) -> HRESULT;
@@ -66,7 +66,7 @@ com_interface! {
 
 iid!(pub IID_IDxcIncludeHandler = 0x7f61_fc7d, 0x950d, 0x467f, 0xb3, 0xe3, 0x3c, 0x02, 0xfb, 0x49, 0x18, 0x7c);
 com_interface! {
-    interface IDxcIncludeHandler: IUnknown{
+    interface IDxcIncludeHandler: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcIncludeHandler,
         vtable: IDxcIncludeHandlerVtbl,
         fn load_source(filename: LPCWSTR, include_source: *mut *mut IDxcBlob) -> HRESULT;
@@ -82,7 +82,7 @@ pub struct DxcDefine {
 
 iid!(pub IID_IDxcCompiler = 0x8c21_0bf3, 0x011f, 0x4422, 0x8d, 0x70, 0x6f, 0x9a, 0xcb, 0x8d, 0xb6, 0x17);
 com_interface! {
-    interface IDxcCompiler: IUnknown{
+    interface IDxcCompiler: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcCompiler,
         vtable: IDxcCompilerVtbl,
         fn compile(
@@ -94,7 +94,7 @@ com_interface! {
             arg_count: u32,
             defines: *const DxcDefine,
             def_count: u32,
-            include_handler: *const c_void,
+            include_handler: *const IDxcIncludeHandler,
             result: *mut *mut IDxcOperationResult) -> HRESULT;
 
         fn preprocess(
@@ -104,7 +104,7 @@ com_interface! {
             arg_count: u32,
             defines: *const DxcDefine,
             def_count: u32,
-            include_handler: *const c_void,
+            include_handler: *const IDxcIncludeHandler,
             result: *mut *mut IDxcOperationResult) -> HRESULT;
 
         fn disassemble(
@@ -115,7 +115,7 @@ com_interface! {
 
 iid!(pub IID_IDxcCompiler2 = 0xA005_A9D9, 0xB8BB, 0x4594, 0xB5, 0xC9, 0x0E, 0x63, 0x3B, 0xEC, 0x4D, 0x37);
 com_interface! {
-    interface IDxcCompiler2: IDxcCompiler, IUnknown{
+    interface IDxcCompiler2: IDxcCompiler, IDxcUnknownShim, IUnknown {
         iid: IID_IDxcCompiler2,
         vtable: IDxcCompiler2Vtbl,
 
@@ -128,7 +128,7 @@ com_interface! {
             arg_count: u32,
             defines: *const DxcDefine,
             def_count: u32,
-            include_handler: *const c_void,
+            include_handler: *const IDxcIncludeHandler,
             result: *mut *mut IDxcOperationResult,
             debug_blob_name: *mut LPWSTR,
             debug_blob: *mut *mut IDxcBlob) -> HRESULT;
@@ -137,7 +137,7 @@ com_interface! {
 
 iid!(pub IID_IDxcLinker = 0xF1B5_BE2A, 0x62DD, 0x4327, 0xA1, 0xC2, 0x42, 0xAC, 0x1E, 0x1E, 0x78, 0xE6);
 com_interface! {
-    interface IDxcLinker: IUnknown{
+    interface IDxcLinker: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcLinker,
         vtable: IDxcLinkerVtbl,
 
@@ -162,7 +162,7 @@ pub const DXC_VALIDATOR_FLAGS_VALID_MASK: u32 = 0x7;
 
 iid!(pub IID_IDxcValidator = 0xA6E8_2BD2, 0x1FD7, 0x4826, 0x98, 0x11, 0x28, 0x57, 0xE7, 0x97, 0xF4, 0x9A);
 com_interface! {
-    interface IDxcValidator: IUnknown{
+    interface IDxcValidator: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcValidator,
         vtable: IDxcValidatorVtbl,
 
@@ -172,7 +172,7 @@ com_interface! {
 
 iid!(pub IID_IDxcContainerBuilder = 0x334b_1f50, 0x2292, 0x4b35, 0x99, 0xa1, 0x25, 0x58, 0x8d, 0x8c, 0x17, 0xfe);
 com_interface! {
-    interface IDxcContainerBuilder: IUnknown{
+    interface IDxcContainerBuilder: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcContainerBuilder,
         vtable: IDxcContainerBuilderVtbl,
 
@@ -185,7 +185,7 @@ com_interface! {
 
 iid!(pub IID_IDxcAssembler = 0x091f_7a26, 0x1c1f, 0x4948, 0x90, 0x4b, 0xe6, 0xe3, 0xa8, 0xa7, 0x71, 0xd5);
 com_interface! {
-    interface IDxcAssembler: IUnknown{
+    interface IDxcAssembler: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcAssembler,
         vtable: IDxcAssemblerVtbl,
 
@@ -195,7 +195,7 @@ com_interface! {
 
 iid!(pub IID_IDxcContainerReflection = 0xd2c2_1b26, 0x8350, 0x4bdc, 0x97, 0x6a, 0x33, 0x1c, 0xe6, 0xf4, 0xc5, 0x4c);
 com_interface! {
-    interface IDxcContainerReflection: IUnknown{
+    interface IDxcContainerReflection: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcContainerReflection,
         vtable: IDxcContainerReflectionVtbl,
 
@@ -210,7 +210,7 @@ com_interface! {
 
 iid!(pub IID_IDxcOptimizerPass = 0xAE2C_D79F, 0xCC22, 0x453F, 0x9B, 0x6B, 0xB1, 0x24, 0xE7, 0xA5, 0x20, 0x4C);
 com_interface! {
-    interface IDxcOptimizerPass: IUnknown{
+    interface IDxcOptimizerPass: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcOptimizerPass,
         vtable: IDxcOptimizerPassVtbl,
 
@@ -224,7 +224,7 @@ com_interface! {
 
 iid!(pub IID_IDxcOptimizer = 0x2574_0E2E, 0x9CBA, 0x401B, 0x91, 0x19, 0x4F, 0xB4, 0x2F, 0x39, 0xF2, 0x70);
 com_interface! {
-    interface IDxcOptimizer: IUnknown{
+    interface IDxcOptimizer: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcOptimizer,
         vtable: IDxcOptimizerVtbl,
 
@@ -245,7 +245,7 @@ pub const DXC_VERSION_INFO_FLAGS_INTERNAL: u32 = 2; // Internal Validator (non-s
 
 iid!(pub IID_IDxcVersionInfo = 0xb04f_5b50, 0x2059, 0x4f12, 0xa8, 0xff, 0xa1, 0xe0, 0xcd, 0xe1, 0xcc, 0x7e);
 com_interface! {
-    interface IDxcVersionInfo: IUnknown{
+    interface IDxcVersionInfo: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcVersionInfo,
         vtable: IDxcVersionInfoVtbl,
 
@@ -256,7 +256,7 @@ com_interface! {
 
 iid!(pub IID_IDxcVersionInfo2 = 0xfb69_04c4, 0x42f0, 0x4b62, 0x9c, 0x46, 0x98, 0x3a, 0xf7, 0xda, 0x7c, 0x83);
 com_interface! {
-    interface IDxcVersionInfo2: IUnknown{
+    interface IDxcVersionInfo2: IDxcUnknownShim, IUnknown {
         iid: IID_IDxcVersionInfo2,
         vtable: IDxcVersionInfo2Vtbl,
 
@@ -264,12 +264,12 @@ com_interface! {
     }
 }
 
-iid!(pub CLSID_DxcCompiler = 0x73e22d93, 0xe6ce, 0x47f3, 0xb5, 0xbf, 0xf0, 0x66, 0x4f, 0x39, 0xc1, 0xb0);
-iid!(pub CLSID_DxcLinker = 0xef6a8087, 0xb0ea, 0x4d56, 0x9e, 0x45, 0xd0, 0x7e, 0x1a, 0x8b, 0x78, 0x6);
-iid!(pub CLSID_DxcDiaDataSource = 0xcd1f6b73, 0x2ab0, 0x484d, 0x8e, 0xdc, 0xeb, 0xe7, 0xa4, 0x3c, 0xa0, 0x9f );
-iid!(pub CLSID_DxcLibrary = 0x6245d6af, 0x66e0, 0x48fd, 0x80, 0xb4, 0x4d, 0x27, 0x17, 0x96, 0x74, 0x8c);
-iid!(pub CLSID_DxcValidator = 0x8ca3e215, 0xf728, 0x4cf3, 0x8c, 0xdd, 0x88, 0xaf, 0x91, 0x75, 0x87, 0xa1 );
-iid!(pub CLSID_DxcAssembler = 0xd728db68, 0xf903, 0x4f80, 0x94, 0xcd, 0xdc, 0xcf, 0x76, 0xec, 0x71, 0x51);
-iid!(pub CLSID_DxcContainerReflection = 0xb9f54489, 0x55b8, 0x400c, 0xba, 0x3a, 0x16, 0x75, 0xe4, 0x72, 0x8b, 0x91);
-iid!(pub CLSID_DxcOptimizer = 0xae2cd79f, 0xcc22, 0x453f, 0x9b, 0x6b, 0xb1, 0x24, 0xe7, 0xa5, 0x20, 0x4c);
-iid!(pub CLSID_DxcContainerBuilder = 0x94134294, 0x411f, 0x4574, 0xb4, 0xd0, 0x87, 0x41, 0xe2, 0x52, 0x40, 0xd2 );
+iid!(pub CLSID_DxcCompiler = 0x73e2_2d93, 0xe6ce, 0x47f3, 0xb5, 0xbf, 0xf0, 0x66, 0x4f, 0x39, 0xc1, 0xb0);
+iid!(pub CLSID_DxcLinker = 0xef6a_8087, 0xb0ea, 0x4d56, 0x9e, 0x45, 0xd0, 0x7e, 0x1a, 0x8b, 0x78, 0x6);
+iid!(pub CLSID_DxcDiaDataSource = 0xcd1f_6b73, 0x2ab0, 0x484d, 0x8e, 0xdc, 0xeb, 0xe7, 0xa4, 0x3c, 0xa0, 0x9f );
+iid!(pub CLSID_DxcLibrary = 0x6245_d6af, 0x66e0, 0x48fd, 0x80, 0xb4, 0x4d, 0x27, 0x17, 0x96, 0x74, 0x8c);
+iid!(pub CLSID_DxcValidator = 0x8ca3_e215, 0xf728, 0x4cf3, 0x8c, 0xdd, 0x88, 0xaf, 0x91, 0x75, 0x87, 0xa1 );
+iid!(pub CLSID_DxcAssembler = 0xd728_db68, 0xf903, 0x4f80, 0x94, 0xcd, 0xdc, 0xcf, 0x76, 0xec, 0x71, 0x51);
+iid!(pub CLSID_DxcContainerReflection = 0xb9f5_4489, 0x55b8, 0x400c, 0xba, 0x3a, 0x16, 0x75, 0xe4, 0x72, 0x8b, 0x91);
+iid!(pub CLSID_DxcOptimizer = 0xae2c_d79f, 0xcc22, 0x453f, 0x9b, 0x6b, 0xb1, 0x24, 0xe7, 0xa5, 0x20, 0x4c);
+iid!(pub CLSID_DxcContainerBuilder = 0x9413_4294, 0x411f, 0x4574, 0xb4, 0xd0, 0x87, 0x41, 0xe2, 0x52, 0x40, 0xd2 );

commit f1ad454f57d559af5b2c1f46f1f8cd72a7409c7c

Linux: util: BSTR contains a wide string instead of UTF-8
Resulting strings were only one character long because the second byte
for ASCII text is NULL.

On Linux DXC properly uses wchar_t which is 32-bit instead of 16-bit.

Fixes: 94fbad7 ("Support libdxcompiler.so on Linux (#5)")

examples/intellisense-tu.rs
@@ -32,6 +32,7 @@ fn main() {
 
         let name = cursor.get_display_name().unwrap();
         println!("Name {:?}", name);
+        assert_eq!(name, "copy.hlsl");
 
         let cursor_kind = cursor.get_kind().unwrap();
         println!("CursorKind {:?}", cursor_kind);
@@ -41,6 +42,19 @@ fn main() {
     }
 
     let child_cursors = cursor.get_all_children().unwrap();
+
+    assert_eq!(
+        child_cursors[0].get_display_name(),
+        Ok("g_input".to_owned())
+    );
+    assert_eq!(
+        child_cursors[1].get_display_name(),
+        Ok("g_output".to_owned())
+    );
+    assert_eq!(
+        child_cursors[2].get_display_name(),
+        Ok("copyCs(uint3)".to_owned())
+    );
 
     for child_cursor in child_cursors {
         let range = child_cursor.get_extent().unwrap();

src/utils.rs
@@ -13,16 +13,19 @@ pub(crate) fn from_wide(wide: LPWSTR) -> String {
     unsafe {
         widestring::WideCStr::from_ptr_str(wide)
             .to_string()
-            .expect("utf16 decode failed")
+            .expect("widestring decode failed")
     }
 }
 
 #[cfg(windows)]
 pub(crate) fn from_bstr(string: BSTR) -> String {
     unsafe {
-        let len = SysStringLen(string);
-        let slice: &[WCHAR] = ::std::slice::from_raw_parts(string, len as usize);
-        let result = String::from_utf16(slice).unwrap();
+        let len = SysStringLen(string) as usize;
+
+        let result = widestring::WideCStr::from_ptr_with_nul(string, len)
+            .to_string()
+            .expect("widestring decode failed");
+
         SysFreeString(string);
         result
     }
@@ -30,10 +33,14 @@ pub(crate) fn from_bstr(string: BSTR) -> String {
 
 #[cfg(not(windows))]
 pub(crate) fn from_bstr(string: BSTR) -> String {
-    // TODO (Marijn): This does NOT cover embedded NULLs:
-    // https://docs.microsoft.com/en-us/windows/win32/api/oleauto/nf-oleauto-sysstringlen#remarks
-    // Fortunately BSTRs are only used in names currently, which _likely_ don't include NULL characters (like binary data)
-    let result = from_lpstr(string as LPSTR);
+    // TODO (Marijn): This does NOT cover embedded NULLs
+
+    // BSTR contains its size in the four bytes preceding the pointer, in order to contain NULL bytes:
+    // https://docs.microsoft.com/en-us/previous-versions/windows/desktop/automat/bstr
+    // DXC on non-Windows does not adhere to that and simply allocates a buffer without prepending the size:
+    // https://github.com/microsoft/DirectXShaderCompiler/blob/a8d9780046cb64a1cea842fa6fc28a250e3e2c09/include/dxc/Support/WinAdapter.h#L49-L50
+    let result = from_wide(string as LPWSTR);
+
     unsafe { SysFreeString(string) };
     result
 }